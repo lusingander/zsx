92ff664e2f7c2cbc148647adf3aef093769472ca
Kyosuke Fujimoto <kyoro.f@gmail.com>
2023-10-13 08:24:25 +0900

    Implement ListProfiles

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/lusingander/zsx
+
+go 1.20
+
+require (
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	gopkg.in/ini.v1 v1.67.0
+)
+
+require github.com/stretchr/testify v1.8.4 // indirect
---
+++ profile.go
@@ -0,0 +1,111 @@
+package zsx
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/exp/slices"
+	"gopkg.in/ini.v1"
+)
+
+// https://docs.aws.amazon.com/sdkref/latest/guide/file-location.html
+// https://docs.aws.amazon.com/sdkref/latest/guide/file-format.html
+// https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+
+func ListProfiles() ([]string, error) {
+	configs, err := loadConfigs()
+	if err != nil {
+		return nil, err
+	}
+	credentials, err := loadCredentials()
+	if err != nil {
+		return nil, err
+	}
+	return mergedProfiles(configs, credentials), nil
+}
+
+type Config struct {
+	name string
+}
+
+func loadConfigs() ([]*Config, error) {
+	f := func(s *ini.Section) (*Config, bool) {
+		if name, found := strings.CutPrefix(s.Name(), "profile "); found {
+			c := &Config{
+				name,
+			}
+			return c, true
+		}
+		return nil, false
+	}
+	path, err := resolveFilePath(".aws", "config")
+	if err != nil {
+		return nil, err
+	}
+	return load(path, f)
+}
+
+type Credential struct {
+	name string
+}
+
+func loadCredentials() ([]*Credential, error) {
+	f := func(s *ini.Section) (*Credential, bool) {
+		c := &Credential{
+			s.Name(),
+		}
+		return c, true
+	}
+	path, err := resolveFilePath(".aws", "credentials")
+	if err != nil {
+		return nil, err
+	}
+	return load(path, f)
+}
+
+func resolveFilePath(elem ...string) (string, error) {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	elem = append([]string{dir}, elem...)
+	return filepath.Join(elem...), nil
+}
+
+func load[T any](path string, f func(*ini.Section) (T, bool)) ([]T, error) {
+	cfg, err := ini.Load(path)
+	if err != nil {
+		return nil, err
+	}
+	ss := cfg.Sections()
+
+	xs := make([]T, 0, len(ss))
+	for _, s := range ss {
+		name := s.Name()
+		if name == ini.DefaultSection {
+			continue
+		}
+		if x, ok := f(s); ok {
+			xs = append(xs, x)
+		}
+	}
+	return xs, nil
+}
+
+func mergedProfiles(configs []*Config, credentials []*Credential) []string {
+	profiles := make([]string, 0)
+	for _, c := range configs {
+		if slices.Contains(profiles, c.name) {
+			continue
+		}
+		profiles = append(profiles, c.name)
+	}
+	for _, c := range credentials {
+		if slices.Contains(profiles, c.name) {
+			continue
+		}
+		profiles = append(profiles, c.name)
+	}
+	return profiles
+}
